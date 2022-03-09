88b8db3d064e2d84e8e17b5325f59f54c578e063
Kevin Quan <8071508+kgquan@users.noreply.github.com>
2022-03-09 15:31:06 -0700

    Add handlebars as a dependency; send customized emails to user when signing up and confirming email

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,22 +3,70 @@ import EmailProvider from 'next-auth/providers/email';
 import nodemailer from 'nodemailer';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
+import Handlebars from 'handlebars';
+import { readFileSync } from 'fs';
+import path from 'path';
 
 const prisma = new PrismaClient();
+
+const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_SERVER_HOST,
+    port: process.env.EMAIL_SERVER_PORT,
+    auth: {
+        user: process.env.EMAIL_SERVER_USER,
+        pass: process.env.EMAIL_SERVER_PASSWORD,
+    },
+    secure: false,
+})
+
+const emailsDir = path.resolve(process.cwd(), 'emails');
+
+const sendVerificationRequest = ({ identifier, url }) => {
+    const emailFile = readFileSync(path.join(emailsDir, 'confirm-email.html'), {
+        encoding: 'utf8',
+    });
+
+    const emailTemplate = Handlebars.compile(emailFile);
+    transporter.sendMail({
+        from: `"✨ SupaVacation" ${process.env.EMAIL_FROM}`,
+        to: identifier,
+        subject: 'Your sigin-in link for SupaVacation',
+        html: emailTemplate({
+            base_url: process.env.NEXTAUTH_URL,
+            signin_url: url,
+            email: identifier,
+        }),
+    });
+}
+
+const sendWelcomeEmail = async ({ user }) => {
+    const { email } = user;
+
+    try {
+        const emailFile = readFileSync(path.join(emailsDir, 'welcome.html'), {
+            encoding: 'utf8',
+        });
+
+        const emailTemplate = Handlebars.compile(emailFile);
+        await transporter.sendMail({
+            from: `"✨ SupaVacation" ${process.env.EMAIL_FROM}`,
+            to: email,
+            subject: 'Welcome to SupaVacation! 🎉',
+            html: emailTemplate({
+                base_url: process.env.NEXTAUTH_URL,
+                support_email: 'support@themodern.dev',
+            }),
+        });
+    } catch(error) {
+        console.log(`❌ Unable to send welcome email to user (${email})`);
+    }
+}
 
 export default NextAuth({
   providers: [
     EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: Number(process.env.EMAIL_SERVER_PORT),
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
-      from: process.env.EMAIL_FROM,
-      maxAge: 10 * 60, // Magic links are valid for 10 min only
+        maxAge: 10 * 60,
+        sendVerificationRequest,
     }),
   ],
   adapter: PrismaAdapter(prisma),
@@ -28,5 +76,5 @@ export default NextAuth({
       error: '/',
       verifyRequest: '/',
   },
+  events: { createUser: sendWelcomeEmail }
 });
-
